8e9e474648eb3aa9e08369c82838c4200dd485ee
Egor Krivonos <karl11karle@gmail.com>
2023-04-23 20:02:44 +0300

    overall commit

---
+++ src/main/java/ru/job4j/oop/Story.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Story {
+    public static void main(String[] args) {
+        Pioneer petya = new Pioneer();
+        Girl girl = new Girl();
+        Wolf wolf = new Wolf();
+        girl.help(petya);
+        petya.kill(wolf);
+    }
+}
