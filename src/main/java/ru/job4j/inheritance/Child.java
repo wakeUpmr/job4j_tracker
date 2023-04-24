32e9b2ea430309f703d5951a7691ac2359ec6943
Egor Krivonos <karl11karle@gmail.com>
2023-04-24 19:52:26 +0300

    constructor

---
+++ src/main/java/ru/job4j/inheritance/Child.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class Child extends Parent {
+    private String patronymic;
+
+    public Child(String name, int age, String patronymic) {
+        super(name, age);
+        this.patronymic = patronymic;
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/Parent.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class Parent {
+
+    private String name;
+    private int age;
+
+    public Parent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
