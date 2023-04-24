b12855627b4234d653b5f85dc755df10d119052d
Egor Krivonos <karl11karle@gmail.com>
2023-04-24 19:46:30 +0300

    Классы и объекты

---
+++ src/main/java/ru/job4j/oop/Local.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Local {
+    private String name = "Egor";
+
+    public void getFullName() {
+        final String surname = "Krivonos";
+
+        class FullName {
+            public void printFullName() {
+                System.out.println(name + " " + surname);
+            }
+        }
+
+        FullName fullName = new FullName();
+        fullName.printFullName();
+    }
+
+    public static void main(String[] args) {
+        Local local = new Local();
+        local.getFullName();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Main.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        truck.accelerate();
+        truck.steer();
+        truck.brake();
+    }
+
+}
