f22f14584559cfb6e5741342c7458bf199686edc
Egor Krivonos <karl11karle@gmail.com>
2023-04-28 14:25:05 +0300

    7. Метод удаления заявки Tracker.delete 3

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,7 +59,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = false;
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, items.length - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[items.length - 1] = null;
             size--;
             rsl = true;
