c395693b10c0d022bfbd85175f7eefe14c9751a5
Egor Krivonos <karl11karle@gmail.com>
2023-05-12 02:21:01 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("element was not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] value = {"apple", "samsung", "honor", "huawei", "asus", "lenovo", "nokia"};
+            System.out.println(indexOf(value, "lg"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
