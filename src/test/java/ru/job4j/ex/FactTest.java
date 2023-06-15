aef343f2a78f3114c5f6e382c4aecfb12d9ff0dc
Egor Krivonos <karl11karle@gmail.com>
2023-06-15 18:35:35 +0300

    cant say

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactTest {
+
     @Test
     public void whenException() {
         IllegalArgumentException exception = assertThrows(
