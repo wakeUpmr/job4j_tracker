2cbe4e86bc2afa826af13f278d9362ae1c3cc5c2
wakeUpmr <egohio.work@gmail.com>
2023-07-08 10:18:31 +0300

    0. Map, HashMap 2

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("egohio.work@gmail.com", "Krivonos Egor Olegovich");
+        map.put("egohio.work@gmail.com", "Krivonos Yakov Olegovich");
+        map.put("ktulh0@gmail.com", "Krivonos Nikolay Olegovich");
+        map.put("karl11karl11@gmail.com", "Ivanov Stepan Maksimovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(value);
